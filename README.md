dc7f892ac451913d23b37d731cbf7f6ca28fe42b
KristaCalleja <kristacm@gmail.com>
2021-08-18 22:05:02 -0300

    Update

--- README.md
+++ README.md
@@ -49,9 +49,12 @@ It was totally unnecessary for me to use JavaScript for the creation of the pseu
 document.styleSheets[1].addRule('footer::before', `content:url('${imageArray[0].src}');`);
 ```
 
+__UPDATE__
+Ultimately, I had to remove the `.src` on the iteration, since this was not used once I changed to array literal notation (see more below).
+
 As you can see, I saved all the images in an array and iterated over each one and rendering them through interpolation.
 
-The creation of an array for images wasn't as straightforward either. To access the image in root you need to set the source through `imageArray[8].src`.
+The creation of an array for images wasn't as straightforward either. To access the image in root you need to set the source through `imageArray[8].src`. __See above__
 
 Of course, I also needed to make sure that the page remained responsive, therefore I used if statements to check the window for size and render the correct image if it matches.
 
@@ -59,6 +62,10 @@ Of course, I also needed to make sure that the page remained responsive, therefo
 // Set media query
 var x = window.matchMedia('(min-width: 900px)');
 ```
+
+__UPDATE__
+
+Upon testing in Chrome, the `.matchMedia` method was not compatible. It was recommended to use the `userAgentData` read-only property which returns a boolean as to whether the user is accessing from desktop or mobile. Although the docs state that this is not fully compatible with all browsers as it is still experimental, it worked on both Firefox and Chrome on my machine.
 
 The aim of applying JS for this relatively simple task was inspired from the desire to write less CSS code (for fun, because I do enjoy it!). It  turns out I had to write just as much JS code, however it was easily done with the Visual Studio Code plugin Text Pastry and ample use of multi-carret selection.
 
@@ -70,7 +77,7 @@ Of course, this is a work in progress and still needs some tweaking. I also need
 
 - [Accessing Pseudo-elements through JS](https://medium.com/how-tos-for-coders/https-medium-com-kaushalshah1307-get-pseudo-elements-using-javascript-b64c88a96c46) - A useful tool for the future.
 - [Stackoverflow varied solutions](https://stackoverflow.com/questions/5041494/selecting-and-manipulating-css-pseudo-elements-such-as-before-and-after-usin) - Suggestions on how to select and manipulate pseudo-elements through JS.
-- [Creating an array of images](https://www.codegrepper.com/code-examples/javascript/array+of+images+javascript) 
+- [Creating an array of images](https://www.codegrepper.com/code-examples/javascript/array+of+images+javascript) __UPDATE__ this resource is not recommended any longer. After passing my code through JSHint [JSHint](https://jshint.com) I learnt that array literal notation is preferred using empty brackets, like so: `newArray = []`. Therefore the images and their source needed less code, so that's a double thumbs up!
 - [Media Queries with JavaScript](https://www.w3schools.com/howto/howto_js_media_queries.asp)
 - [Text Pastry](https://twitter.com/wesbos/status/926484635437228038?lang=en) - Inspiration on using this plugin for some quick insertion of incremental numbers.
 
--- custom.js
+++ custom.js
@@ -1,98 +1,88 @@
 /*jshint esversion: 6 */
 
 // Put all images in an array
-const  imageArray = new Array();
-imageArray[0] = new Image();
-imageArray[0].src  = 'images/bg-footer-top-desktop.svg';
-imageArray[1] = new Image();
-imageArray[1].src  = 'images/bg-footer-top-mobile.svg';
-imageArray[2] = new Image();
-imageArray[2].src  = 'images/bg-section-bottom-desktop-1.svg';
-imageArray[3] = new Image();
-imageArray[3].src  = 'images/bg-section-bottom-desktop-2.svg';
-imageArray[4] = new Image();
-imageArray[4].src  = 'images/bg-section-bottom-mobile-1.svg';
-imageArray[5] = new Image();
-imageArray[5].src  = 'images/bg-section-bottom-mobile-2.svg';
-imageArray[6] = new Image();
-imageArray[6].src  = 'images/bg-section-top-desktop-1.svg';
-imageArray[7] = new Image();
-imageArray[7].src  = 'images/bg-section-top-desktop-2.svg';
-imageArray[8] = new Image();
-imageArray[8].src  = 'images/bg-section-top-mobile-1.svg';
-imageArray[9] = new Image();
-imageArray[9].src  = 'images/bg-section-top-mobile-2.svg';
+const imageArray = ['images/bg-footer-top-desktop.svg', // [0]
+    'images/bg-footer-top-mobile.svg', // [1] 
+    'images/bg-section-bottom-desktop-1.svg', // [2] 
+    'images/bg-section-bottom-desktop-2.svg', // [3]
+    'images/bg-section-bottom-mobile-1.svg', // [4]
+    'images/bg-section-bottom-mobile-2.svg', // [5]
+    'images/bg-section-top-desktop-1.svg', // [6]
+    'images/bg-section-top-desktop-2.svg', // [7]
+    'images/bg-section-top-mobile-1.svg', // [8]
+    'images/bg-section-top-mobile-2.svg', // [9]
+];
 
 // Set media query
-var x = window.matchMedia('(min-width: 900px)');
+const isMobile = navigator.userAgent.mobile;
+
 
 // Select the doms where the pseudo-elements need to be rendered
-function footerEl(x){
-    if (x.matches){
-        document.styleSheets[1].addRule('footer::before', `content:url('${imageArray[0].src}');`);
-        document.styleSheets[1].addRule('footer::before', 'display:flex');
-        document.styleSheets[1].addRule('footer::before', 'overflow:hidden');
-    } else {
-        document.styleSheets[1].addRule('footer::before', `content:url('${imageArray[1].src}');`);
+function footerEl(){
+    if (!isMobile){ // true to 'desktop'
+    document.styleSheets[1].addRule('footer::before', `content:url('${imageArray[0]}');`);
+    document.styleSheets[1].addRule('footer::before', 'display:flex');
+    document.styleSheets[1].addRule('footer::before', 'overflow:hidden');
+} else {
+    document.styleSheets[1].addRule('footer::before', `content:url('${imageArray[1]}');`);
         document.styleSheets[1].addRule('footer::before', 'display:flex');
         document.styleSheets[1].addRule('footer::before', 'overflow:hidden');
     }
-}
+};
 // Attached to section div
-function topSectionEl(x){
-    if (x.matches){
-        document.styleSheets[1].addRule('section::before', `content:url('${imageArray[6].src}');`);
+function topSectionEl(){
+    if (!isMobile){
+        document.styleSheets[1].addRule('section::before', `content:url('${imageArray[6]}');`);
         document.styleSheets[1].addRule('section::before', 'display:flex');
         document.styleSheets[1].addRule('section::before', 'overflow:hidden');
     } else {
-        document.styleSheets[1].addRule('section::before', `content:url('${imageArray[8].src}');`);
+        document.styleSheets[1].addRule('section::before', `content:url('${imageArray[8]}');`);
         document.styleSheets[1].addRule('section::before', 'display:flex');
         document.styleSheets[1].addRule('section::before', 'overflow:hidden');
     }
-}
-function bottomSectionEl(x){
-    if (x.matches){
-        document.styleSheets[1].addRule('section::after', `content:url('${imageArray[3].src}');`);
+};
+function bottomSectionEl(){
+    if (!isMobile){
+        document.styleSheets[1].addRule('section::after', `content:url('${imageArray[3]}');`);
         document.styleSheets[1].addRule('section::after', 'display:flex');
         document.styleSheets[1].addRule('section::after', 'overflow:hidden');
     } else {
-        document.styleSheets[1].addRule('section::after', `content:url('${imageArray[5].src}');`);
+        document.styleSheets[1].addRule('section::after', `content:url('${imageArray[5]}');`);
         document.styleSheets[1].addRule('section::after', 'display:flex');
         document.styleSheets[1].addRule('section::after', 'overflow:hidden');
     }
-}
+};
 // Attached to last ribbon
-function topRibbonEl(x){
-    if (x.matches){
-        document.styleSheets[1].addRule('.ribbon::before', `content:url('${imageArray[7].src}');`);
+function topRibbonEl(){
+    if (!isMobile){
+        document.styleSheets[1].addRule('.ribbon::before', `content:url('${imageArray[7]}');`);
         document.styleSheets[1].addRule('.ribbon::before', 'display:flex');
         document.styleSheets[1].addRule('.ribbon::before', 'overflow:hidden');
     } else {
-        document.styleSheets[1].addRule('.ribbon::before', `content:url('${imageArray[9].src}');`);
+        document.styleSheets[1].addRule('.ribbon::before', `content:url('${imageArray[9]}');`);
         document.styleSheets[1].addRule('.ribbon::before', 'display:flex');
         document.styleSheets[1].addRule('.ribbon::before', 'overflow:hidden');
     }
-}
+};
 // Attached to first ribbon
-function bottomRibbonEl(x){
-    if (x.matches){
-        document.styleSheets[1].addRule('.ribbon::after', `content:url('${imageArray[2].src}');`);
+function bottomRibbonEl(){
+    if (!isMobile){
+        document.styleSheets[1].addRule('.ribbon::after', `content:url('${imageArray[2]}');`);
         document.styleSheets[1].addRule('.ribbon::after', 'display:flex');
         document.styleSheets[1].addRule('.ribbon::after', 'overflow:hidden');
     } else {
-        document.styleSheets[1].addRule('.ribbon::after', `content:url('${imageArray[4].src}');`);
+        document.styleSheets[1].addRule('.ribbon::after', `content:url('${imageArray[4]}');`);
         document.styleSheets[1].addRule('.ribbon::after', 'display:flex');
         document.styleSheets[1].addRule('.ribbon::after', 'overflow:hidden');
     }
-}
+};
 
-footerEl(x);
-topSectionEl(x);
-bottomSectionEl(x);
-topRibbonEl(x);
-bottomRibbonEl(x);
-x.addListener(footerEl, topSectionEl, bottomSectionEl, topRibbonEl, bottomRibbonEl);
-
+// Function invocation
+footerEl();
+topSectionEl();
+bottomSectionEl();
+topRibbonEl();
+bottomRibbonEl();
 
 // Error handler
 function handleError(err){
